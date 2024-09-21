da1a0653eeb16d314909eace8b1c899a9c2fb86c
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-09-21 12:31:58 -0400

    mistake

---
+++ frontend/src/components/GoogleFrame24.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style.css";
+
+
+export const WireframingEmail = () => {
+  return (
+    <div className="BoxGoogle">
+    <img className="googleLogo" src="" />
+    <div style="color: #8A8A8A; font-size: 13px; font-family: Montserrat; font-weight: 600; word-wrap: break-word">Google</div>
+</div>
+  );
+};
+
+export default WireframingEmail;
